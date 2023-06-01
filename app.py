5f2e74958c743606d4cab8fcb823168755c79a3d
Badreesh Shetty <badreeshshetty@gmail.com>
2023-06-01 15:59:18 -0400

    updated app.py

--- app.py
+++ app.py
@@ -8,14 +8,10 @@ st.set_page_config(
 
 st.write("# Exploring Economic Indicators and Stock Market Performance of Companies and Pay of CEOs: A Data Visualization Outlook")
 
-st.write("""This a project or analysis that focuses on understanding the relationship between economic indicators, such as GDP growth, inflation rate, and stock market performance of companies, and the compensation of CEOs.
+st.write("""This a project or analysis that focuses on understanding the relationship between economic indicators, such as GDP growth and stock market performance of companies, and the compensation of CEOs.
 The project utilizes data visualization techniques to present the findings in a visually appealing and easy-to-understand manner.
-By analyzing various economic indicators and stock market data, the project aims to uncover potential correlations or patterns between these factors and the pay levels of CEOs. 
-The study may investigate whether there is a relationship between a company's financial performance, as reflected in its stock market performance, and the compensation received by its CEO.
 The use of data visualization in this project helps to enhance the interpretation of complex data sets and allows viewers to grasp the key insights more intuitively. 
-Visual representations such as graphs, charts, or interactive dashboards can present the relationships and trends between economic indicators, stock market performance, and CEO pay in a visually compelling and informative way.
-Overall, this project aims to provide a comprehensive understanding of how economic indicators and stock market performance can influence the compensation of CEOs in different companies. 
-The use of data visualization techniques enhances the clarity and accessibility of the findings, enabling stakeholders to gain valuable insights into these relationships.""")
+Visual representations such as graphs, charts, or interactive dashboards can present the relationships and trends between economic indicators, stock market performance, and CEO pay in a visually compelling and informative way.""")
 
 st.sidebar.success("Select the tool above.")
 
